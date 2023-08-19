367b8067f27875feac70331ebae02692bcca70d4
LiteRedMore (LRM) <141714951+literedmore@users.noreply.github.com>
2023-08-19 21:30:52 +0700

    update dnsseed node

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -162,13 +162,13 @@ public:
         assert(consensus.hashGenesisBlock == uint256S("0x00000d46ffd839235f7b0e582fe5ac1bc9c70ab91f469bd31cda04e14a2ce42b"));
         assert(genesis.hashMerkleRoot == uint256S("0xaeb019c6f3a00f787190e934c29201d7bbbccf824b5d4c7f812386b263823e24"));
 
-        vSeeds.push_back(CDNSSeedData("litered.org", "node.litered.org"));
-        vSeeds.push_back(CDNSSeedData("node2.litered.org", "node2.litered.org"));
-	vSeeds.push_back(CDNSSeedData("node3.litered.org", "node3.litered.org"));
-	vSeeds.push_back(CDNSSeedData("node4.litered.org", "node4.litered.org"));
-	vSeeds.push_back(CDNSSeedData("node5.litered.org", "node5.litered.org"));
-	vSeeds.push_back(CDNSSeedData("node6.litered.org", "node6.litered.org"));
-	vSeeds.push_back(CDNSSeedData("node7.litered.org", "node7.litered.org"));
+        vSeeds.push_back(CDNSSeedData("litered.online", "vps.litered.online"));
+        vSeeds.push_back(CDNSSeedData("dnsseed.litered.online", "dnsseed.litered.online"));
+	vSeeds.push_back(CDNSSeedData("dnsseed1.litered.online", "dnsseed1.litered.online"));
+	vSeeds.push_back(CDNSSeedData("dnsseed2.litered.online", "dnsseed2.litered.online"));
+	vSeeds.push_back(CDNSSeedData("dnsseed3.litered.online", "dnsseed3.litered.online"));
+	vSeeds.push_back(CDNSSeedData("dnsseed4.litered.online", "dnsseed4.litered.online"));
+	vSeeds.push_back(CDNSSeedData("dnsseed5.litered.online", "dnsseed5.litered.online"));
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,49);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,48);
